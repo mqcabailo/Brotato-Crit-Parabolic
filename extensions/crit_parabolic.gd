6d0ecfd93c74cbc33198c3246c2109f9b061aeb5
matthew <mcabailo@gmail.com>
2025-03-19 01:16:00 +0800

    Added initial implementation of Parabolic Crit

---
+++ extensions/crit_parabolic.gd
@@ -0,0 +1,39 @@
+extends "res://entities/units/unit/unit.gd"
+
+
+func take_damage(value: int, args: TakeDamageArgs) -> Array:
+	var hitbox = args.hitbox
+	if hitbox == null:
+		return .take_damage(value, args)
+	
+	else:
+		var hitbox_crit_chance = hitbox.crit_chance
+		var hitbox_crit_damage = hitbox.crit_damage
+		
+		var crit_power = floor(hitbox_crit_chance)
+		var chance_remainder = fmod(hitbox_crit_chance, 1.0)
+		var crit_on_remainder = Utils.get_chance_success(chance_remainder)
+		
+		if crit_on_remainder:
+			crit_power += 1
+		
+		var result: Array
+		
+		if crit_power <= 0:
+			hitbox.crit_chance = 0.0
+			hitbox.crit_damage = 1.0
+			
+			result = .take_damage(value, args)
+			
+		else:
+			var parabolic_crit_damage = round(pow(hitbox_crit_damage + 1.0, crit_power)) as int
+			
+			hitbox.crit_chance = 1.0
+			hitbox.crit_damage = 1.0
+			
+			result = .take_damage(parabolic_crit_damage, args)
+			
+		hitbox.crit_chance = hitbox_crit_chance
+		hitbox.crit_damage = hitbox_crit_damage
+			
+		return result
---
+++ mod_main.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+const QCROMA_CRITPARABOLIC_DIR := "Qcroma-CritParabolic"
+const QCROMA_CRITPARABOLIC_LOG := "Qcroma-CritParabolic:Main"
+
+var mod_dir_path := ""
+var extensions_dir_path := ""
+var translations_dir_path := ""
+
+func _init() -> void:
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().plus_file(QCROMA_CRITPARABOLIC_DIR)
+	extensions_dir_path = mod_dir_path.plus_file("extensions")
+	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("crit_parablic.gd"))
+
+func _ready() -> void:
+	ModLoaderLog.info("Ready!", QCROMA_CRITPARABOLIC_LOG)
